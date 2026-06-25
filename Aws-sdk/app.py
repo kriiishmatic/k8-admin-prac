b81fa483d7459de50941a9ee0456aa6387cb414e
kriiishamtic <kriiishmatic@gmail.com>
2026-06-25 13:42:24 +0530

    all

--- Aws-sdk/app.py
+++ Aws-sdk/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import boto3
 import os
 import logging
+import json
 
 app = Flask(__name__)
 
@@ -12,21 +13,83 @@ SECRET_NAME = os.getenv("SECRET_NAME", "roboshop/dev/mysql_password")
 
 
 def get_secret():
-    client = boto3.client("secretsmanager", region_name=REGION)
+
+    client = boto3.client(
+        "secretsmanager",
+        region_name=REGION
+    )
 
     response = client.get_secret_value(
         SecretId=SECRET_NAME
     )
 
-    return response["SecretString"]
+    return json.loads(response["SecretString"])
+
+
+def get_identity():
+
+    client = boto3.client(
+        "sts",
+        region_name=REGION
+    )
+
+    return client.get_caller_identity()
 
 
 @app.route("/")
 def home():
+
+    return {
+        "application": "AWS SDK Secret Reader",
+        "status": "Running",
+        "available_endpoints": [
+            "/health",
+            "/identity",
+            "/secret"
+        ]
+    }
+
+
+@app.route("/health")
+def health():
+
+    return {
+        "status": "UP"
+    }
+
+
+@app.route("/identity")
+def identity():
+
     try:
+
+        identity = get_identity()
+
+        logging.info(identity)
+
+        return {
+            "status": "SUCCESS",
+            "identity": identity
+        }
+
+    except Exception as e:
+
+        logging.error(str(e))
+
+        return {
+            "status": "FAILED",
+            "error": str(e)
+        }, 500
+
+
+@app.route("/secret")
+def secret():
+
+    try:
+
         secret = get_secret()
 
-        logging.info(f"Retrieved Secret : {secret}")
+        logging.info(secret)
 
         return {
             "status": "SUCCESS",
@@ -43,14 +106,8 @@ def home():
         }, 500
 
 
-@app.route("/health")
-def health():
-    return {
-        "status": "UP"
-    }
-
-
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=8080
